b288238da2ea4ca2f142cae734d0d23ec773ebc9
Ivan Timonin <icormoran@gmail.com>
2026-07-27 21:43:55 +0300

    Update collate function for spectrogram batches

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -4,17 +4,12 @@ import torch
 def collate_fn(dataset_items: list[dict]):
     result_batch = {}
 
-    waveforms = []
-    lengths = []
+    spectrograms = []
     labels = []
     for sample in dataset_items:
-        waveforms.append(sample["data_object"])
-        lengths.append(sample["data_object"].shape[0])
+        spectrograms.append(sample["data_object"])
         labels.append(sample["labels"])
 
-    result_batch["data_object"] = torch.nn.utils.rnn.pad_sequence(
-        waveforms, batch_first=True, padding_value=0.0
-    )
-    result_batch["audio_lengths"] = torch.tensor(lengths)
+    result_batch["data_object"] = torch.stack(spectrograms, dim=0)
     result_batch["labels"] = torch.tensor(labels)
     return result_batch
